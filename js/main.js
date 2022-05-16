966be7481ff451a46ae0b42e8f2becd2f12e1737
Sazib <sazibbanik8770@gmail.com>
2022-05-15 23:59:48 -0700

    complete rent part

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
-$(function () {
-    // js start here........
-});
+// counter_slider start
+window.onload = ()=>{
+    // $(selector).countMe(delay,speed)
+    $("#num1").countMe(40,65);
+    $("#num2").countMe(30, 30);
+    $("#num3").countMe(40, 50);
+    $("#num4").countMe(100,200);
+ }
+ 
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-36251023-1']);
+    _gaq.push(['_setDomainName', 'jqueryscript.net']);
+    _gaq.push(['_trackPageview']);
+  
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+//   counter_slider end
